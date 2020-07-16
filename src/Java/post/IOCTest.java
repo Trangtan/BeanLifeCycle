87a0e5eb0766269250850df46206f14578e4daaa
Trangtan <1311168sd@gmail.com>
2020-07-16 10:40:41 +0800

    Update

--- src/Java/post/IOCTest.java
+++ src/Java/post/IOCTest.java
@@ -1,12 +1,12 @@
 package post;
 
-import init.MainConfigOfLifeCycle;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class IOCTest {
     public static void main(String[] args) {
         ApplicationContext applicationContext =  new AnnotationConfigApplicationContext(MainConfigOfLifeCycle.class);
-
+        ((ConfigurableApplicationContext)applicationContext).close();
     }
 }
--- src/Java/post/MainConfigOfLifeCycle.java
+++ src/Java/post/MainConfigOfLifeCycle.java
@@ -1,6 +1,5 @@
 package post;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
